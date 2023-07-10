066877328c63cb5025372909c5c7eefc1fbee824
Tushar Bhowal <tusharbhowal3211@gmail.com>
2023-07-11 00:45:23 +0530

    third

--- src/components/Home/ScrollDown.jsx
+++ src/components/Home/ScrollDown.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const ScrollDown = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollVariants = {
+    initial: {
+      y: 0,
+    },
+    animate: {
+      y: -scrollY,
+    },
+  };
+
   return (
     <div className="scroll_down">
       <a href="#about" className="mouse_wrapper">
         <span className="home_scroll-name text-black font-medium">Scroll Down</span>
-        <span className="mouse">
+        <motion.span
+          className="mouse"
+          variants={scrollVariants}
+          initial="initial"
+          animate="animate"
+        >
           <span className="wheel"></span>
-        </span>
+        </motion.span>
       </a>
     </div>
   );
